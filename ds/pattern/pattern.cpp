2afe1990f4f86a91d8828ce1bd1205af33f4bfda
Shivam kumar Jha <76209315+Shivamjha12@users.noreply.github.com>
2022-02-19 16:48:28 +0530

    Update pattern.cpp

--- ds/pattern/pattern.cpp
+++ ds/pattern/pattern.cpp
@@ -75,10 +75,12 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    int count = 1;
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
             if(j<=i){
-                cout<<i;
+                cout<<count;
+                count++;
             }
             else{
                 cout<<" ";
