8c3924ef58517b0e398d1e36e5c4930b1574ce86
Shivam kumar Jha <76209315+Shivamjha12@users.noreply.github.com>
2022-02-19 16:42:47 +0530

    Create pattern.cpp

---
+++ ds/pattern/pattern.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main(){
+//     int rows, cols;
+//     cin>>rows>>cols;
+//     for(int i=1; i<=rows; i++){
+//         for(int j=1; j<=cols; j++){
+//             if(i==1 || i==rows || j==1 || j==cols){
+//             cout<<"*";
+//             }
+//             else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+
+//     return 0;
+// }
+
+// int main(){
+//     int cols;
+//     cin>>cols;
+//     for(int i=cols; i>=1; i--){
+//         for(int j=1; j<=i; j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             if(j<=n-i){
+//                 cout<<" ";
+//             }
+//             else{
+//                 cout<<"*";
+//             }
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+// int main(){
+//     int cols;
+//     cin>>cols;
+//     for(int i=cols; i>=1; i--){
+//         for(int j=1; j<=i; j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             if(j<=n-i){
+//                 cout<<" ";
+//             }
+//             else{
+//                 cout<<i;
+//             }
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(j<=i){
+                cout<<i;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+}
