8671ee6c01a2943e2bbd30b18831d898eb585e71
Shivam kumar Jha <76209315+Shivamjha12@users.noreply.github.com>
2023-04-17 14:30:01 +0530

    Update Heap_class.cpp

--- ds/heaps/Heap_class.cpp
+++ ds/heaps/Heap_class.cpp
@@ -11,4 +11,29 @@ class Minheap{
   int rightIndex(int i){ return (2*i+1); }
   int parentIndex(int i){ return (i-1)/2 ;}
   
+  void heapify(int arr[], int n, int i)  
+    {
+      // Your Code Here
+      int li = leftc(i), ri=rightc(i);
+      int smallest = i;
+      if(li<n && arr[li)] < arr[smallest]){
+        //   swap(arr[left(i)], arr[i]);
+          smallest = li;
+      }
+      if(ri<n && arr[ri] < arr[smallest]){
+        //   swap(arr[left(i)], arr[i]);
+          smallest = ri;
+      }
+      if(smallest !=i){
+          swap(arr[i],arr[smallest]);
+          heapify(smallest);
+      }
+    }
+  
+  void insert(int value){
+    size++;
+    arr[size-1]=value;
+    
+  }
+  
 }
